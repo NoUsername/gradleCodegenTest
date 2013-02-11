cc8b68739cbd213c36d4770952ecaf790522a451
NoUsername <paul@paukl.at>
2013-02-11 21:17:49 +0100

    initial test files

---
+++ src/main/java/at/paukl/gradle/Main.java
@@ -0,0 +1,13 @@
+package at.paukl.gradle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    static Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        log.info("working");
+    }
+}
